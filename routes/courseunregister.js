462e6da796de629ad3f432d4b9ff14cac22d5539
hassan876 <hassan876@users.noreply.github.com>
2020-10-25 16:22:25 +0500

    git

--- routes/courseunregister.js
+++ routes/courseunregister.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const RegisterCourse = require('../models/RegisterCourse');
+const Course = require('../models/Course');
 const auth = require('../middlewares/auth');
+const RegisterCourse = require('../models/RegisterCourse');
 const router = express.Router();
-const learner = require('../models/Learner');
 
 /**
- * @route DELETE / api/course
+ * @route DELETE / api/cou
+ * rse
  * @description remove course
  * @access Private
  */
 router.delete('/:id', auth, async (req, res) => {
     try {
-       const courseUnRegister = await RegisterCourse.findById(req.params.id);
-
+        const courseID = req.params.id;
+        const courseUnRegister = await  RegisterCourse.find({"course_id": courseID});
+      
+        console.log(courseUnRegister);
         if (!courseUnRegister) return res.status(404).json({ msg: 'Course with this id does not exists' });
 
-        
+        // Make sure user owns course
         if (courseUnRegister.learner_id.toString() !== req.user.id)
             return res.status(401).json({ msg: 'Not authorized' });
 
@@ -23,6 +26,7 @@ router.delete('/:id', auth, async (req, res) => {
         res.status(200).json({ msg: 'Course successfully unregistered' });
 
     } catch (err) {
+        
         console.error(err.message);
         res.status(500).send('Server error');
     }
